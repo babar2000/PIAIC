2e3757e5ebd589afa5423418f97ad65808c61439
Zain Babar <zainibaba1990@yahoo.com>
2023-07-12 11:00:55 +0500

    AOA

---
+++ 1.store a persons name/Assignment 1/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var a = ("Hello Zain, Would you like to learn typesrcipt");
+console.log(a);
---
+++ 1.store a persons name/Assignment 1/index.ts
@@ -0,0 +1,2 @@
+var a = ("Hello Zain, Would you like to learn typesrcipt");
+console.log(a);
