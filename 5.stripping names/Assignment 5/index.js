178941beff0a59f64d00e7baa01f6c82c1aaaddb
Zain Babar <zainibaba1990@yahoo.com>
2023-07-14 22:43:56 +0500

    Stripping names

---
+++ 5.stripping names/Assignment 5/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let personName = "\t  Zain Babar \n";
+console.log("Name with whitespace:");
+console.log(personName);
+let strippedName = personName.trim();
+console.log("Stripped name:");
+console.log(strippedName);
---
+++ 5.stripping names/Assignment 5/index.ts
@@ -0,0 +1,7 @@
+let personName: string = "\t  Zain Babar \n";
+console.log("Name with whitespace:");
+console.log(personName);
+
+let strippedName: string = personName.trim();
+console.log("Stripped name:");
+console.log(strippedName);
