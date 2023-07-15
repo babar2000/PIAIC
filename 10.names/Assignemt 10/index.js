716c0029c673cbec390c8a997075b056c75b7c27
Zain Babar <zainibaba1990@yahoo.com>
2023-07-15 09:47:49 +0500

    names

---
+++ 10.names/Assignemt 10/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const names = ["Ali", "Imran", "Mushtaq", "Kamran"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 10.names/Assignemt 10/index.ts
@@ -0,0 +1,5 @@
+const names: string[] = ["Ali", "Imran", "Mushtaq", "Kamran"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
