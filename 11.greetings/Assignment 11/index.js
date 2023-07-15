919a10d4459f9a7aa1fc8b998248ec6ee1da41bf
Zain Babar <zainibaba1990@yahoo.com>
2023-07-15 09:51:57 +0500

    Greetings

---
+++ 11.greetings/Assignment 11/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const names = ["Ali", "Imran", "Mushtaq", "Kamran"];
+for (let i = 0; i < names.length; i++) {
+    const message = `Hello, ${names[i]}! How are you today?`;
+    console.log(message);
+}
---
+++ 11.greetings/Assignment 11/index.ts
@@ -0,0 +1,6 @@
+const names: string[] = ["Ali", "Imran", "Mushtaq", "Kamran"];
+
+for (let i = 0; i < names.length; i++) {
+  const message: string = `Hello, ${names[i]}! How are you today?`;
+  console.log(message);
+}
