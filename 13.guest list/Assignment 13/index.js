357f8b62e32b8e88ec79bfe01df72b284c09e6a9
Zain Babar <zainibaba1990@yahoo.com>
2023-07-15 10:01:34 +0500

    guest list

---
+++ 13.guest list/Assignment 13/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const guestList = ["Ali Imran", "Rana Salman", "Saeed Shah"];
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest}, you are cordially invited to dinner. Please join us for an evening of great conversation and delightful food.`);
+});
---
+++ 13.guest list/Assignment 13/index.ts
@@ -0,0 +1,5 @@
+const guestList: string[] = ["Ali Imran", "Rana Salman", "Saeed Shah"];
+
+guestList.forEach((guest: string) => {
+  console.log(`Dear ${guest}, you are cordially invited to dinner. Please join us for an evening of great conversation and delightful food.`);
+});
