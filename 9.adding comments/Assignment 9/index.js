37c292f9730f4e62fd19b6f0d14a3afe09045384
Zain Babar <zainibaba1990@yahoo.com>
2023-07-15 09:42:44 +0500

    adding comments

---
+++ 9.adding comments/Assignment 9/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing favorite number in a variable
+const favoriteNumber = 22;
+// Creating a message using string interpolation to reveal the favorite number
+const message = `My favorite number is ${favoriteNumber}.`;
+// Printing the message to the console
+console.log(message);
---
+++ 9.adding comments/Assignment 9/index.ts
@@ -0,0 +1,8 @@
+// Storing favorite number in a variable
+const favoriteNumber: number = 22;
+
+// Creating a message using string interpolation to reveal the favorite number
+const message: string = `My favorite number is ${favoriteNumber}.`;
+
+// Printing the message to the console
+console.log(message);
